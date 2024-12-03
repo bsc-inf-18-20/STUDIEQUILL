f498e0f08a534bfe20025383aba1a6e4e2dedbc3
bsc-inf-18-20 <bsc-inf-18-20@unima.ac.mw>
2024-12-03 16:50:28 +0000

    removing un used code

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,18 +1,1 @@
-// notes_page.dart
-import 'package:flutter/material.dart';
 
-class NotesPage extends StatelessWidget {
-  const NotesPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-      ),
-      body: const Center(
-        child: Text('This is the Notes Page'),
-      ),
-    );
-  }
-}
